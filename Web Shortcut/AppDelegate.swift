75df2924586cc9cb1a85f2fd5a971934ff2ff9ae
Marc Shearer <marc.shearer@tesco.net>
2019-04-21 05:53:13 +0100

    Initial Commit

---
+++ Web Shortcut/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Web Shortcut
+//
+//  Created by Marc Shearer on 21/04/2019.
+//  Copyright © 2019 Marc Shearer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
